3a08b817c74f21fceb34fcc6444314ef67f9901b
Mociej <maciekkocemba@gmail.com>
2021-11-25 13:28:06 +0100

    add second endpoint

--- src/main/java/ppkwuzad4/ppkwuzad4/Controllers/ConvertFileTypeApi.java
+++ src/main/java/ppkwuzad4/ppkwuzad4/Controllers/ConvertFileTypeApi.java
@@ -12,4 +12,23 @@ public class ConvertFileTypeApi {
         FileSaverApiController fileSaverApiController = new FileSaverApiController();
         return fileSaverApiController.saveStatsToFile(filetype,input);
     }
+
+    @GetMapping("/convertTypeOfStats/{newfiletype}/{oldfiletype}/{input}")
+    public String convertTypeOfStats(@PathVariable String newfiletype, @PathVariable String oldfiletype, @PathVariable String input) {
+        if(!oldfiletype.equals("txt") || !oldfiletype.equals("xml") || !oldfiletype.equals("json") || !oldfiletype.equals("csv")){
+            return "wrong input file types";
+        }
+        if(!newfiletype.equals("txt") || !newfiletype.equals("xml") || !newfiletype.equals("json") || !newfiletype.equals("csv")){
+            return "wrong input file types";
+        }
+
+        FileSaverApiController fileSaverApiController = new FileSaverApiController();
+
+
+
+
+        String result = fileSaverApiController.saveStatsToFile(oldfiletype,input);
+        return result;
+    }
+
 }
